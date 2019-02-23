6b63654cc9ec581363b135c38061f421a61717ce
pillowhead <gabriel.belmonte@gmail.com>
2019-02-23 23:36:01 +0100

    add input field to TodoList

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -9,6 +9,19 @@ function TodoList(props) {
     <div className="todolist">
       <h1>{title}</h1>
 
+      <div class="input-group">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Please, write new task..."
+        />
+        <div class="input-group-append" id="button-addon4">
+          <button class="btn btn-outline-secondary" type="button">
+            Add Task
+          </button>
+        </div>
+      </div>
+
       <ul className="list-group">
         {items.map(item => (
           <TodoItem key={item.id} data={item} />
