af0cfd7db0f7b8e40f628ca38988c400cc5fdf4a
pillowhead <gabriel.belmonte@gmail.com>
2019-02-27 11:14:13 +0100

    clean css file and move state from App to TodoList

--- src/App.js
+++ src/App.js
@@ -1,63 +1,16 @@
-import React, { Component } from "react";
+import React from "react";
 import TodoList from "./components/TodoList";
-import TodoService from "./components/TodoService";
 
-class App extends Component {
-  constructor() {
-    super();
+function App() {
+  let title = "ToDo list";
 
-    this.state = {
-      filter: "all",
-      items: []
-    };
-  }
-
-  componentWillMount() {
-    this.load();
-  }
-
-  async load() {
-    let res = await TodoService.getTodoList(this.state.filter);
-
-    this.setState({
-      items: res.data
-    });
-  }
-
-  render() {
-    let title = "ToDo list";
-
-    return (
-      <div className="container">
-        <div className="row">
-          <TodoList
-            title={title}
-            items={this.state.items}
-            addNew={this.addNew.bind(this)}
-            filter={this.state.filter}
-            changeFilter={this.changeFilter.bind(this)}
-          />
-        </div>
+  return (
+    <div className="container">
+      <div className="row">
+        <TodoList title={title} />
       </div>
-    );
-  }
-
-  addNew(text) {
-    let nextId = this.state.items.length + 1; //basic nextId concept
-    let item = {
-      id: nextId,
-      text: text
-    };
-    let updatedList = this.state.items.concat([item]);
-
-    this.setState({
-      items: updatedList
-    });
-  }
-
-  changeFilter(filter) {
-    this.setState({ filter });
-  }
+    </div>
+  );
 }
 
 export default App;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,40 +1,75 @@
-import React from "react";
+import React, { Component } from "react";
 import TodoItem from "./TodoItem";
 import Footer from "./Footer";
 import InputForm from "./InputForm";
 import Filter from "./Filter";
+import TodoService from "./TodoService";
 
-function TodoList(props) {
-  const { title, items, addNew, filter, changeFilter } = props;
+class TodoList extends Component {
+  constructor(props) {
+    super(props);
 
-  const filteredItems = applyFilter(items, filter);
+    this.state = {
+      filter: "all",
+      items: []
+    };
+  }
 
-  return (
-    <div className="todolist">
-      <h1>{title}</h1>
-      <InputForm addNew={addNew} />
+  componentWillMount() {
+    this.load();
+  }
 
-      <ul className="list-group">
-        {filteredItems.map(item => (
-          <TodoItem key={item.id} data={item} />
-        ))}
-      </ul>
-      <Footer items={items} />
-      <Filter change={changeFilter} />
-    </div>
-  );
-}
+  async load() {
+    let res = await TodoService.getTodoList(this.state.filter);
 
-function applyFilter(unfilteredItems, filter) {
-  switch (filter) {
-    case "completed":
-      return unfilteredItems.filter(item => item.completed === true);
+    this.setState({
+      items: res.data
+    });
+  }
 
-    case "active":
-      return unfilteredItems.filter(item => item.completed !== true);
+  addNew(text) {
+    const data = { text: text };
+    TodoService.createTodoItem(data);
+  }
 
-    default:
-      return unfilteredItems;
+  changeFilter(filter) {
+    this.setState({ filter });
+  }
+
+  applyFilter(unfilteredItems, filter) {
+    switch (filter) {
+      case "completed":
+        return unfilteredItems.filter(item => item.completed === true);
+
+      case "active":
+        return unfilteredItems.filter(item => item.completed !== true);
+
+      default:
+        return unfilteredItems;
+    }
+  }
+
+  render() {
+    const title = this.props.title;
+    const filter = this.state.filter;
+    const items = this.state.items;
+
+    const filteredItems = this.applyFilter(items, filter);
+
+    return (
+      <div className="todolist">
+        <h1>{title}</h1>
+        <InputForm addNew={this.addNew.bind(this)} />
+
+        <ul className="list-group">
+          {filteredItems.map(item => (
+            <TodoItem key={item.id} data={item} />
+          ))}
+        </ul>
+        <Footer items={items} />
+        <Filter change={this.changeFilter.bind(this)} />
+      </div>
+    );
   }
 }
 
